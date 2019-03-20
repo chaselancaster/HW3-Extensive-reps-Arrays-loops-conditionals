129b3469f45d8a2c1c37aa197cde1634a9a0f493
chaselancaster <chaseblancaster@gmail.com>
2019-03-20 16:56:08 -0700

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 // Easy Going 
 
-for (let i = 1; i <= 20; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
 
 // Get Even 
 
-for (let i = 0; i <= 200; i += 2) {
-    console.log(i); 
-}
+// for (let i = 0; i <= 200; i += 2) {
+//     console.log(i); 
+// }
 
 // Excited Kitten 
 
-for (let i = 0; i < 20; i++) {
-    let random = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away...'];
-    console.log('Love me, pet me! HSSSSSS!')
-    if (i % 2 === 0) {
-        console.log(random[Math.floor(Math.random() * 3)]);
-    }
-}
+// for (let i = 0; i < 20; i++) {
+//     let random = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away...'];
+//     console.log('Love me, pet me! HSSSSSS!')
+//     if (i % 2 === 0) {
+//         console.log(random[Math.floor(Math.random() * 3)]);
+//     }
+// }
 
 // Fizz Buzz 
 
@@ -27,14 +27,52 @@ for (let i = 0; i < 20; i++) {
 // If statement that checks if number is divisible by 5 - logs "Buzz"
 // If state that checks if number is divisible by 3 AND 5 - logs "FizzBuzz"
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Getting to Know You 
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1
+
+// kenny.splice(0, 1, "Gameboy");
+
+// 2 
+
+// for (let i = 0; i < jimClark.length; i++) {
+//     if (!isNaN(jimClark[i])) {
+//         jimClark[i]++
+//     }
+// }
+
+// 3
+
+// ryan.splice(2, 1, "Gotham City");
+
+// 4
+
+// reuben.pop();
+// reuben.push("Chicago");
+
+// 5
+
+jimHaff.pop().push("LA, SF, SD");
+
+
+
+
+
