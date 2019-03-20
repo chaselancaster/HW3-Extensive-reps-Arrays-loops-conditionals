194b11172bb865be4c97c6e10441aa99dc88e29b
chaselancaster <chaseblancaster@gmail.com>
2019-03-20 16:04:20 -0700

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -9,3 +9,13 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 0; i <= 200; i += 2) {
     console.log(i); 
 }
+
+// Excited Kitten 
+
+for (let i = 0; i < 20; i++) {
+    let random = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away...'];
+    console.log('Love me, pet me! HSSSSSS!')
+    if (i % 2 === 0) {
+        console.log(random[Math.floor(Math.random() * 3)]);
+    }
+}
